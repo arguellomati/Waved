55bc116e83a9ed50b7f0975ce6b944f114001ab7
ArguelloMati <arguellomati12@gmail.com>
2023-01-12 14:24:20 -0300

    "Agregado funcionalidad carrito"

--- Waved-main/main.js
+++ Waved-main/main.js
@@ -1,139 +1,250 @@
-const menuEmail = document.querySelector(".navbar-email");
-const desktop = document.querySelector(".desktop");
-const menuCarrito = document.querySelector(".navbar-cart");
-const productDetailClose = document.querySelector(".product-close")
-const shoppingCart = document.querySelector(".product")
-const cardsContainer = document.querySelector(".cards-container")
-const productDetail = document.querySelector(".product-detail")
-const darkenScreen = document.querySelector(".darken");
+let emailMenu           = document.querySelector('.navbar-email');
+let desktopMenu         = document.querySelector('.desktop-menu');
+let iconMenuMobile      = document.querySelector('.menu');
+let mobileMenu          = document.querySelector('.mobile-menu');
+let iconShopping        = document.querySelector('.navbar-shopping-cart');
+let asideShopping       = document.querySelector('.product-detail');
+let divcardscontainer   = document.querySelector('.cards-container')
+let productDetailLeft   = document.querySelector('.product-detail-left')
+let productDetailClose  = document.querySelector('.product-detail-close')
+let myordercontentContainer = document.querySelector('.my-order-content')
+let countCarrito        = document.querySelector('.navbar-shopping-cart div')
+let totalCarrito        = document.querySelector('.total')
+let modal               = document.querySelector('.modal')
+let cerrarModal         = document.querySelector('.boton-modal')
+let productImageInfor   = document.querySelector('.product-detail-left > img:nth-child(2)')
+let labelPriceInfo      = document.querySelector('.product-info-left p:nth-child(1)')
+let labelNameInfo       = document.querySelector('.product-info-left p:nth-child(2)')
+let labelInforInfo      = document.querySelector('.product-info-left p:nth-child(3)')
+let buttonInfo          = document.querySelector('.add-to-cart-button')
+let productosEnCarrito  = []
+let productoACarrito    = []
+
+countCarrito.innerText = document.querySelectorAll('.shopping-cart').length
+totalCarrito.innerText = '$0.00'
+
+emailMenu.addEventListener('click', toggleMenu);
+iconMenuMobile.addEventListener('click', toggleMenuMobile);
+iconShopping.addEventListener('click', toggleAsideShopping)
+productDetailClose.addEventListener('click',closeProductDetail)
+buttonInfo.addEventListener('click', butonClick)
+cerrarModal.addEventListener('click', closeModal)
 
 
-
-menuEmail.addEventListener("click", toggleDesktop);
-menuCarrito.addEventListener("click", toggleCarritoAside);
-productDetailClose.addEventListener("click", closeProductDetail);
-
-function darkenChange(){
-    darkenScreen.classList.toggle("inactive");
+function butonClick(){
+    agregarCarrito(productoACarrito)
+}
+function toggleMenu(){
+    productDetailLeft.classList.add('inactive')
+    asideShopping.classList.add('inactive');
+    desktopMenu.classList.toggle('inactive');
 }
 
-function toggleDesktop() {
-    desktop.classList.toggle("inactive");
-    shoppingCart.classList.add("inactive")
-    productDetail.classList.add("inactive")
-
-    const isMenuDesktop = !desktop.classList.contains("inactive")
-
-    if(isMenuDesktop){
-        darkenScreen.classList.remove("inactive");
-    } else {
-        darkenScreen.classList.add("inactive");
-    }
+function toggleMenuMobile(){
+    productDetailLeft.classList.add('inactive')
+    asideShopping.classList.add('inactive');
+    mobileMenu.classList.toggle('inactive')
+    
 }
 
-
-function toggleCarritoAside () {
-    shoppingCart.classList.toggle("inactive");
-    desktop.classList.add("inactive")
-    productDetail.classList.add("inactive")
-
-    const isShoppingCart= !productDetail.classList.contains("inactive");
-
-    if(isShoppingCart){
-        darkenScreen.classList.remove("inactive");
-    } else {
-        darkenScreen.classList.add("inactive");
-    }
+function toggleAsideShopping(){
+    productDetailLeft.classList.add('inactive')
+    mobileMenu.classList.add('inactive');
+    desktopMenu.classList.add('inactive')
+    asideShopping.classList.toggle('inactive')
 }
 
 function openProductDetail(){
-    productDetail.classList.remove("inactive")
-    darkenScreen.classList.add("inactive");
+    asideShopping.classList.add('inactive');
+    mobileMenu.classList.add('inactive');
+    desktopMenu.classList.add('inactive');
+    productDetailLeft.classList.remove('inactive')
 }
 
-function closeProductDetail() {
-    productDetailClose.classList.add("inactive")
-    productDetail.classList.add("inactive")
-    desktop.classList.add("inactive");
-    darkenScreen.classList.remove("inactive");
+function closeProductDetail(){
+    productoACarrito = []
+    asideShopping.classList.add('inactive');
+    mobileMenu.classList.add('inactive');
+    desktopMenu.classList.add('inactive');
+    productDetailLeft.classList.add('inactive')
 }
 
-const productList=[];
+function verificarProducto(nameProduct){
+    return productosEnCarrito.some(function(articulo){
+        return articulo === nameProduct
+    })
+}
+
+function closeModal(){
+    modal.style.display = 'none'
+}
+
+let productList = []
 productList.push({
-    name:'Not feeling it',
-    price:5000,
-    image:'./logos/notfeeling.PNG',
-});
+    name:"Salty!",
+    price:4000,
+    image:"./logos/salty.PNG",
+    desc: ' Stay saltyyy! Remeras creadas con la mejor calidad del mercado, drops unicos e irrepetibles, pocas unidades que hacen mantener su originalidad y exclusividad.'
+})
 productList.push({
-    name:'Rainbow',
-    price:5000,
-    image:'./logos/rainbow.PNG',
-});
+    name:"Paradise!",
+    price:4000,
+    image: "./logos/paradise.PNG",
+    desc: 'Enjoy the fucking paradise! Remeras creadas con la mejor calidad del mercado, drops unicos e irrepetibles, pocas unidades que hacen mantener su originalidad y exclusividad.'
+})
 productList.push({
-    name:'Make money',
-    price:5000,
-    image:'./logos/makemoney.PNG',
-});
+    name:"Ride Or Die",
+    price:4000,
+    image:"./logos/rideordie.PNG",
+    desc: 'Ride or die! Remeras creadas con la mejor calidad del mercado, drops unicos e irrepetibles, pocas unidades que hacen mantener su originalidad y exclusividad.'
+})
 productList.push({
-    name:'Trust no one',
-    price:5000,
-    image:'./logos/trustnoone.PNG',
-});
+    name:"Aloha!",
+    price:4000,
+    image:"./logos/aloha!.PNG",
+    desc: 'Alohaa! Remeras creadas con la mejor calidad del mercado, drops unicos e irrepetibles, pocas unidades que hacen mantener su originalidad y exclusividad.'
+})
 productList.push({
-    name:'Gangsters dont cry',
-    price:5000,
-    image:'./logos/gangster.PNG',
-});
+    name:"Taste your words",
+    price:4000,
+    image:"./logos/tastewords.PNG",
+    desc: 'Always taste your words first. Remeras creadas con la mejor calidad del mercado, drops unicos e irrepetibles, pocas unidades que hacen mantener su originalidad y exclusividad.'
+})
 productList.push({
-    name:'Paradise',
-    price:5000,
-    image:'./logos/paradise.PNG',
-});
+    name:"Gansters dont cry",
+    price:6000,
+    image:"./logos/gangster.png",
+    desc: 'A real gangster never cry. buzos creados con la mejor calidad del mercado, drops unicos e irrepetibles, pocas unidades que hacen mantener su originalidad y exclusividad.'
+})
 productList.push({
-    name:'Salty!',
-    price:5000,
-    image:'./logos/salty.PNG',
-});
+    name:"Rainbow",
+    price:6000,
+    image:"./logos/rainbow.png",
+    desc: 'Look at the Rainbow, ahhh beautiful shit. buzos creados con la mejor calidad del mercado, drops unicos e irrepetibles, pocas unidades que hacen mantener su originalidad y exclusividad.'
+})
 productList.push({
-    name:'Taste your words',
-    price:5000,
-    image:'./logos/tastewords.PNG',
-});
+    name:"Not feeling it",
+    price:6000,
+    image:"./logos/notfeeling.png",
+    desc: 'Just a bad day, Dont disturb. buzos creados con la mejor calidad del mercado, drops unicos e irrepetibles, pocas unidades que hacen mantener su originalidad y exclusividad.'
+})
 productList.push({
-    name:'Ride Or Die!',
-    price:5000,
-    image:'./logos/rideordie.PNG',
-});
+    name:"Make money not friends",
+    price:6000,
+    image:"./logos/makemoney.png",
+    desc: 'The money ALWAYS first. buzos creados con la mejor calidad del mercado, drops unicos e irrepetibles, pocas unidades que hacen mantener su originalidad y exclusividad.'
+})
 productList.push({
-    name:'Aloha!',
-    price:5000,
-    image:'./logos/aloha!.PNG',
-});
-function renderProducts(arr){
-    for(product of arr){
-        const productCard=document.createElement('div');
-        productCard.classList.add('product-card');
-        const productImg=document.createElement('img');
-        productImg.setAttribute('src',product.image);
-        productImg.addEventListener("click", openProductDetail)
-        const productInfo=document.createElement('div');
-        productInfo.classList.add('product-info');
-        const productInfoDiv=document.createElement('div');
-        const productPrice=document.createElement('p');
-        productPrice.innerText='$'+product.price;
-        const productName=document.createElement('p');
-        productName.innerText=product.name;
-        productInfoDiv.appendChild(productPrice);
-        productInfoDiv.appendChild(productName);
-        const productInfoFigure=document.createElement('figure');
-        const productImgCart=document.createElement('img');
-        productImgCart.setAttribute('src','./icons/bt_add_to_cart.svg');
-        productInfoFigure.appendChild(productImgCart);
-        productInfo.appendChild(productInfoDiv);
-        productInfo.appendChild(productInfoFigure);
-        productCard.appendChild(productImg);
-        productCard.appendChild(productInfo);
-        cardsContainer.appendChild(productCard);
+    name:"Trust no one",
+    price:6000,
+    image:"./logos/trustnoone.png",
+    desc: 'Dont trust anyone. buzos creados con la mejor calidad del mercado, drops unicos e irrepetibles, pocas unidades que hacen mantener su originalidad y exclusividad.'
+})
+
+function renderListProduct(productList){
+    for (const product of productList) {
+        let divproductCard = document.createElement('div')
+        let imgproductcard = document.createElement('img')
+        let divproductinfo = document.createElement('div')
+        let divdivproductinfo = document.createElement('div')
+        let pprice = document.createElement('p')
+        let pname = document.createElement('p')
+        let figureproductinfo = document.createElement('figure')
+        let imgproductinfo = document.createElement('img')
+        divproductCard.classList.add('product-card')
+        imgproductcard.setAttribute('src', product.image)
+        divproductinfo.classList.add('product-info')
+        pprice.innerText ='$' + product.price
+        pname.innerText = product.name
+        imgproductinfo.setAttribute('src', './icons/bt_add_to_cart.svg')
+        
+        figureproductinfo.appendChild(imgproductinfo)
+        divdivproductinfo.appendChild(pprice)
+        divdivproductinfo.appendChild(pname)
+
+        divproductinfo.appendChild(divdivproductinfo)
+        divproductinfo.appendChild(figureproductinfo)
+
+        divproductCard.appendChild(imgproductcard)
+        divproductCard.appendChild(divproductinfo)
+
+        divcardscontainer.appendChild(divproductCard)
+
+        imgproductcard.addEventListener('click', function(){
+            mostrarInfoProduct(product.image, product.price, product.name, product.desc)
+            openProductDetail()
+        });
+
+
+        imgproductinfo.addEventListener('click', function(){
+            let productoAgregado = []
+            productoAgregado.push({
+                name: product.name,
+                price: product.price,
+                image: product.image
+            })
+            agregarCarrito(productoAgregado)
+        })
     }
 }
-renderProducts(productList);
+
+/* Funcion para agregar productos al carrito */
+function agregarCarrito(producto){
+    if(verificarProducto(producto[0].name)){
+        productoACarrito= []
+        return modal.style.display = 'grid'
+    }
+
+    /* Creacion de elemtentos HTML */
+    let divShoppingCart = document.createElement('div')
+    let figureShoppingCart = document.createElement('figure')
+    let imgfigureShoppingCart = document.createElement('img')
+    let pnameShoppingCart = document.createElement('p')
+    let ppriceShoppingCart = document.createElement('p')
+    let imgcloseShoppingCart = document.createElement('img')
+
+    /* Se agrega contenido a cada uno de los elementos */
+    divShoppingCart.classList.add('shopping-cart')
+    imgfigureShoppingCart.setAttribute('src', producto[0].image)
+    pnameShoppingCart.innerText = producto[0].name
+    ppriceShoppingCart.innerText ='$' + producto[0].price
+    imgcloseShoppingCart.setAttribute('src','./icons/icon_close.png')
+    imgcloseShoppingCart.classList.add('removeList')
+
+    /* Se insertan los documentos */
+    figureShoppingCart.appendChild(imgfigureShoppingCart)
+    divShoppingCart.appendChild(figureShoppingCart)
+    divShoppingCart.appendChild(pnameShoppingCart)
+    divShoppingCart.appendChild(ppriceShoppingCart)
+    divShoppingCart.appendChild(imgcloseShoppingCart)
+    myordercontentContainer.appendChild (divShoppingCart)
+
+    /* Se agregan la cantidad de productos ademas de la suma de precios */
+    countCarrito.innerText = document.querySelectorAll('.shopping-cart').length
+    totalCarrito.innerText = '$' + (Number(totalCarrito.innerText.substring(1)) + Number(producto[0].price))
+    productosEnCarrito.push(producto[0].name)
+    
+    /* Funcion para quitar productos del carrito */
+    imgcloseShoppingCart.addEventListener('click', function(){
+        divShoppingCart.remove()
+        countCarrito.innerText = document.querySelectorAll('.shopping-cart').length
+        totalCarrito.innerText = '$' + (Number(totalCarrito.innerText.substring(1)) - Number(producto[0].price))
+        productosEnCarrito.splice(productosEnCarrito.indexOf(producto[0].name),1)
+    }) 
+
+}
+
+function mostrarInfoProduct(imagen, precio, nombre, descripcion){
+    productImageInfor.setAttribute('src', imagen)
+    labelPriceInfo.innerText = '$' + precio
+    labelNameInfo.innerText = nombre
+    labelInforInfo.innerText = descripcion
+    productoACarrito = []
+    productoACarrito.push({
+        name: nombre,
+        price: precio,
+        image: imagen
+    })
+}
+
+renderListProduct(productList);
